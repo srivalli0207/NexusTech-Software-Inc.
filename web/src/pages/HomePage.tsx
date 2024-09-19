8ca74f3f1e57fcc7dc151f4cb2336c9443622b77
AndyCChen <acchen@cpp.edu>
2024-09-19 15:02:25 -0700

    add split button for displaying tables

--- web/src/pages/HomePage.tsx
+++ web/src/pages/HomePage.tsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
-import reactLogo from '../assets/react.svg'
-import viteLogo from '/vite.svg'
+import SplitButton from '../components/SplitButton'
 import '../styles/App.css'
 import '../styles/index.css'
 
@@ -10,11 +9,24 @@ type TestData =
   text: string,
 }
 
+type ButtonOptions = 
+{
+  buttonName: string,
+  fetchURL: string,
+}
+
+const splitButtonOptions: ButtonOptions[] = [
+  {buttonName: "Fetch1", fetchURL: "http://ec2-13-57-195-163.us-west-1.compute.amazonaws.com/api/test/posts"},
+  {buttonName: "Fetch2", fetchURL: "http://ec2-13-57-195-163.us-west-1.compute.amazonaws.com/api/test/posts"},
+  {buttonName: "Fetch3", fetchURL: "http://ec2-13-57-195-163.us-west-1.compute.amazonaws.com/api/test/posts"},
+  {buttonName: "etc", fetchURL: "http://ec2-13-57-195-163.us-west-1.compute.amazonaws.com/api/test/posts"}
+];
+
 export default function HomePage() {
   const [testData, setTestData] = useState<TestData[]>([])
 
-  const fetchTest = async () => {
-    const fetch_res = await fetch("http://ec2-13-57-195-163.us-west-1.compute.amazonaws.com/api/test/posts")
+  const fetchOnClick = async (index: number) => {
+    const fetch_res = await fetch(splitButtonOptions[index].fetchURL)
     const data: TestData[] = await fetch_res.json()
     console.log(data)
     setTestData([...testData, ...data])
@@ -22,31 +34,7 @@ export default function HomePage() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={fetchTest}>
-          Fetch Me
-        </button>
-        {
-          testData.map((value, index) => {
-            return <p key={index}>Count: {index}, Author: {value.author}, Text: {value.text}</p>
-          })
-        }
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <SplitButton options={splitButtonOptions.map((value) => value.buttonName)} handleClickCallback={fetchOnClick}/> 
     </>
   )
 }
