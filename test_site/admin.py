7aae8325fd78e0490b7ad836690e5c6800cee829
Andy Chen <acchen@cpp.edu>
2024-09-19 19:11:09 -0700

    register models into admin site

--- test_site/admin.py
+++ test_site/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+
+admin.site.register(User)
+admin.site.register(Comment)
+admin.site.register(Session)
+admin.site.register(Post)
+admin.site.register(PostMedia)
+admin.site.register(Forum)
+admin.site.register(DirectMessage)
+admin.site.register(Report)
