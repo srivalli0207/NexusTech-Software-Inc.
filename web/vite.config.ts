46a8b89fa568371359d6f5f2c993c4ccd6a3d1b8
Andy Chen <acchen@cpp.edu>
2024-09-25 17:50:44 -0700

    configure local proxy so frontend and backend can be accessed on the same domain

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -4,4 +4,18 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      // direct /api to django dev server
+      '/api': {
+        target: 'http://127.0.0.1:8000',
+        changeOrigin: true,
+      },
+      // for serving static files for django admin site
+      '/static': {
+        target: 'http://127.0.0.1:8000',
+        changeOrigin: true,
+      }
+    }
+  },
 })
