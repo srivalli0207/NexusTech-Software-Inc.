42a57e6bd73f2a5f835902e870cdd825de1447a8
entriphy <1747088+entriphy@users.noreply.github.com>
2024-10-08 22:55:32 -0700

    Update message models

--- test_site/models/__init__.py
+++ test_site/models/__init__.py
@@ -1,6 +1,6 @@
 from .comment import Comment, CommentLike
 from .forum import Forum, ForumFollow, ForumModerator
-from .message import DirectMessage
+from .message import MessageConversation, MessageConversationMember, Message, MessageMedia
 from .post import Post, PostMedia, PostLike, PostBookmark, PostUserTag
 from .report import Report
 from .user import UserProfile, Follow, UserMutedWord, UserBlock, UserSettings
--- test_site/models/message.py
+++ test_site/models/message.py
@@ -1,12 +1,36 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 from .user import UserProfile
 
-class DirectMessage(models.Model):
+
+class MessageConversation(models.Model):
+    conversation_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=128, null=True, default=None)
+    group = models.BooleanField(default=False)
+    creator = models.ForeignKey(UserProfile, on_delete=models.CASCADE, related_name="creator", null=True, default=None)
+    members = models.ManyToManyField(UserProfile, through="MessageConversationMember")
+
+
+class MessageConversationMember(models.Model):
+    conversation = models.ForeignKey(MessageConversation, on_delete=models.CASCADE)
+    user = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+
+
+class Message(models.Model):
     message_id = models.AutoField(primary_key=True)
-    from_user = models.ForeignKey(UserProfile, on_delete=models.CASCADE, related_name="sending_user")
-    to_user = models.ForeignKey(UserProfile, on_delete=models.CASCADE, related_name="receiving_user")
-    message = models.TextField()
-    sent = models.DateTimeField()
+    user = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    conversation = models.ForeignKey(MessageConversation, on_delete=models.CASCADE)
+    text = models.TextField(null=True)
+    sent = models.DateTimeField(auto_now_add=True)
 
 
-# TODO: media
+class MessageMedia(models.Model):
+    class MediaType(models.TextChoices):
+        PHOTO = "PHOTO", _("PHOTO")
+        VIDEO = "VIDEO", _("VIDEO")
+
+    message = models.ForeignKey(Message, on_delete=models.CASCADE)
+    media_type = models.CharField(max_length=5, choices=MediaType.choices)
+    url = models.URLField()
+    index = models.IntegerField(default=0)
+
