f6526b1b71e1cd8eb6aa476842014fce0f5c9a8a
Jfortner0 <Jackiefort@icloud.com>
2024-11-13 10:04:12 -0800

    finished forum follow and unfollow tests

--- test_site/tests.py
+++ test_site/tests.py
@@ -261,12 +261,18 @@ class TestForums(TestCase):
     def setUp(self):
         self.forum_owner = UserProfile.create_user("owner", "owner@gmail.com", "forumowner")
         self.test_forum = Forum.objects.create(name="test_forum", description="this is a test forum", creator=self.forum_owner)
+        self.user1 = UserProfile.create_user("user1", "user1@gmail.com", "user1123")
+
     
     def test_forum_follow(self):
-        ...
+        ForumFollow.objects.create(user=self.user1, forum=self.test_forum)
+        self.assertTrue(ForumFollow.objects.filter(user=self.user1, forum=self.test_forum).exists(), "User 1 follows a forum")
         
     def test_forum_unfollow(self):
-        ...
+        test_forum_follow = ForumFollow.objects.create(user=self.user1, forum=self.test_forum)
+        self.assertTrue(ForumFollow.objects.filter(user=self.user1, forum=self.test_forum).exists(), "User 1 follows a forum")
+        test_forum_follow.delete()
+        self.assertFalse(ForumFollow.objects.filter(user=self.user1, forum=self.test_forum).exists(), "User 1 no longer follows a forum")
 
     def test_create_forum(self):
         ...
