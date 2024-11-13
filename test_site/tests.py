f2253790707de193cc54538ef8489b3b6b490d02
Jfortner0 <Jackiefort@icloud.com>
2024-11-13 09:30:21 -0800

    debug TestReport

--- test_site/tests.py
+++ test_site/tests.py
@@ -236,22 +236,23 @@ class TestMessages(TestCase):
     
 class TestReport(TestCase):
     def setUp(self):
-        self.user1 = UserProfile.create_user("reporter" "treportgmail.com", "testing123")
+        self.user1 = UserProfile.create_user("reporter", "report@gmail.com", "testing123")
         self.user2 = UserProfile.create_user("reported", "reported@gmail.com", "testing456")
         self.post = Post.objects.create(user=self.user1, text="This is a post.")
 
-    def test_reort_user(self):
-        test_report = Report.objects.create(report_user=self.user1, reported_user= self.user2, reason="test reason")
+    def test_report_user(self):
+        test_report = Report.objects.create(report_user=self.user1, reported_user= self.user2, reason="test reason", reported_at="2023-11-13 14:25:45")
         self.assertTrue(Report.objects.filter(report_id=test_report.report_id))
 
 
     def test_report_post(self):
-        test_report_post = Report.objects.create(report_user=self.user1, reported_post=self.post, reason="test post reason")
+        test_report_post = Report.objects.create(report_user=self.user1, reported_post=self.post, reason="test post reason", reported_at="2023-11-13 14:25:45")
         self.assertTrue(Report.objects.filter(report_id=test_report_post.report_id))
 
     def test_report_comment(self):
         comment = Comment.objects.create(post=self.post, user=self.user1, content="Test comment")
-        test_report_comment = Report.objects.create(report_user=self.user1, reported_comment=comment, reason="test comment reason")
+        test_report_comment = Report.objects.create(report_user=self.user1, reported_comment=comment, reason="test comment reason", reported_at="2023-11-13 14:25:45")
+        self.assertTrue(Report.objects.filter(report_id=test_report_comment.report_id))
 
         
 class TestForums(TestCase):
